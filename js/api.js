af669bb153026690004e61c840b48967d3a145d2
Nichole Nicolini <120338930+nicholetzs@users.noreply.github.com>
2023-08-09 11:40:41 -0300

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,18 @@
+const apiKey = 'YOUR KEY API';
+
+    async function fetchAPOD() {
+      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
+      const data = await response.json();
+
+      const apodImage = document.getElementById('apod-image');
+      const apodTitle = document.getElementById('apod-title');
+      const apodExplanation = document.getElementById('apod-explanation');
+      const loadButton = document.getElementById('loadButton');
+      const apodContainer = document.getElementById('apodContainer');
+      loadButton.style.display = 'none';
+      apodContainer.style.display = 'block';
+  
+      apodImage.src = data.url;
+      apodTitle.textContent = data.title;
+      apodExplanation.textContent = data.explanation;
+    }
