e0e93448f0448c9c10cf9812fa9a8dcc068e5d3f
Nichole <nchnicolini@gmail.com>
2024-08-31 22:30:43 -0300

    nasa

--- js/api.js
+++ js/api.js
@@ -1,18 +1,20 @@
-const apiKey = 'YOUR KEY API';
+const apiKey = "qJsdEhgxPj4vcnCGHb0kjndBit1Ku2RTehYfNRBH";
 
-    async function fetchAPOD() {
-      const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`);
-      const data = await response.json();
+async function fetchAPOD() {
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+  );
+  const data = await response.json();
 
-      const apodImage = document.getElementById('apod-image');
-      const apodTitle = document.getElementById('apod-title');
-      const apodExplanation = document.getElementById('apod-explanation');
-      const loadButton = document.getElementById('loadButton');
-      const apodContainer = document.getElementById('apodContainer');
-      loadButton.style.display = 'none';
-      apodContainer.style.display = 'block';
-  
-      apodImage.src = data.url;
-      apodTitle.textContent = data.title;
-      apodExplanation.textContent = data.explanation;
-    }
+  const apodImage = document.getElementById("apod-image");
+  const apodTitle = document.getElementById("apod-title");
+  const apodExplanation = document.getElementById("apod-explanation");
+  const loadButton = document.getElementById("loadButton");
+  const apodContainer = document.getElementById("apodContainer");
+  loadButton.style.display = "none";
+  apodContainer.style.display = "block";
+
+  apodImage.src = data.url;
+  apodTitle.textContent = data.title;
+  apodExplanation.textContent = data.explanation;
+}
